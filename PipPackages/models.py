eb27297fad2879a31e4411c409c4c2cc2af750f9
keraattin <kerem-oruc@outlook.com>
2024-02-12 20:42:35 +0300

    Pip Packages Models File Created

---
+++ PipPackages/models.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+# Libraries
+##############################################################################
+from mongoengine import (
+    Document, DictField, ReferenceField, DateTimeField, ListField,
+    EmbeddedDocument, EmbeddedDocumentField, StringField
+)
+##############################################################################
