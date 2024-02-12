8541fedc0dfb9931db0eeac4476eb3699b95898f
keraattin <kerem-oruc@outlook.com>
2024-02-12 20:44:20 +0300

    PipPackage Model Added

--- PipPackages/models.py
+++ PipPackages/models.py
@@ -6,4 +6,29 @@ from mongoengine import (
     Document, DictField, ReferenceField, DateTimeField, ListField,
     EmbeddedDocument, EmbeddedDocumentField, StringField
 )
+
+##############################################################################
+
+
+# Models
+##############################################################################
+class PipPackage(EmbeddedDocument):
+    name            = StringField()
+    version         = StringField()
+
+    def __eq__(self, other):
+        return (
+            self.name == other.name and
+            self.version == other.version
+        )
+
+    def serialize(self):
+        data = {
+            "name": self.name,
+            "version": self.version
+        }
+        return data
+
+    def __str__(self):
+        return str(self.serialize())
 ##############################################################################
