73c62473473ddbac52f45c91a4567cb7e28140d4
keraattin <kerem-oruc@outlook.com>
2024-03-17 17:56:49 +0300

    Get All Npm Packages Endpoint Added

--- NpmPackages/routes.py
+++ NpmPackages/routes.py
@@ -2,7 +2,10 @@
 
 # Libraries
 ##############################################################################
-from flask import Blueprint
+from flask import Blueprint, jsonify
+
+from .models import InstalledNpmPackages
+from Shared.validators import auth_token_required
 ##############################################################################
 
 # Blueprint
@@ -12,5 +15,17 @@ sys_npm_pkgs_bp = Blueprint('sys_npm_pkgs_blueprint', __name__)
 
 # Routes
 ##############################################################################
+
+# Get All Npm Packages from All Agents
+@sys_npm_pkgs_bp.route('/', methods=['GET'])
+@auth_token_required
+def get_all_installed_npm_packages():
+    try:
+        # Get All Npm Packages from DB
+        all_npm_pkgs = InstalledNpmPackages.objects()
+        # Serialize & Return
+        return [info.serialize() for info in all_npm_pkgs] 
+    except Exception as e:
+        return jsonify({"error":str(e)}), 500
 
 ##############################################################################
