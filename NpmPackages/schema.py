8ae22680ffb6b093003aa2053c29beb9b5d2f666
keraattin <kerem-oruc@outlook.com>
2024-03-22 15:26:00 +0300

    Npm Schemas created

---
+++ NpmPackages/schema.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+# Libraries
+##############################################################################
+from marshmallow import Schema, fields
+##############################################################################
+
+# NpmPackage Schema
+##############################################################################
+class NpmPackageSchema(Schema):
+    name         = fields.Str(required=True)
+    version      = fields.Str(required=True)
+##############################################################################
+
+# Register Schema
+##############################################################################
+class RegisterSchema(Schema):
+    is_installed = fields.Boolean(required=True)
+    pip_packages = fields.List(fields.Nested(NpmPackageSchema))
+##############################################################################
