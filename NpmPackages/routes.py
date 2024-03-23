79b065b5a94deddb550a0eb1142f14d1d491ad65
keraattin <kerem-oruc@outlook.com>
2024-03-23 15:46:11 +0300

    Npm Package Register endpoint added

--- NpmPackages/routes.py
+++ NpmPackages/routes.py
@@ -2,10 +2,16 @@
 
 # Libraries
 ##############################################################################
-from flask import Blueprint, jsonify
+from flask import Blueprint, request, jsonify
+from datetime import datetime
+from marshmallow import ValidationError
 
-from .models import InstalledNpmPackages
-from Shared.validators import auth_token_required
+from .models import InstalledNpmPackages, ChangeLogInstalledNpmPackages
+from .schema import RegisterSchema
+from Shared.validators import agent_token_required, auth_token_required
+
+from Agents.helper_funcs import get_id_by_token
+from Agents.models import Agent
 ##############################################################################
 
 # Blueprint
@@ -40,4 +46,77 @@ def get_npm_packages_by_agent_id(agent_id):
     except Exception as e:
         return jsonify({"Message":str(e)}), 404
 
+# Register
+@sys_npm_pkgs_bp.route('/', methods=['POST'])
+@agent_token_required
+def register():
+    # Get Agent Token by Authorization Header
+    agent_token = request.headers.get('Authorization')
+    # Get Agent ID by Token
+    agent_id = get_id_by_token(agent_token)
+    # Get Agent by Agent ID
+    agent = Agent.objects(id=agent_id).first()
+
+    try:
+        # Load and validate the JSON request using the schema
+        data = RegisterSchema().load(request.json)
+    except ValidationError as e:
+        # Return validation errors as a JSON response with a 400 status code
+        return jsonify({'error': e.messages}), 400
+        
+    # Get if record already exist
+    npm_pkgs = InstalledNpmPackages.objects(agent=agent).first()
+
+    if npm_pkgs:
+        # UPDATE If System Information data already exist
+        try:
+            # Create InstalledNpmPackages object to Compare
+            new_npm_pkgs_obj = InstalledNpmPackages(**data)
+            new_npm_pkgs_obj.agent = agent
+
+            # Find deleted, new, and updated npm_pkgs
+            new_npm_pkgs, deleted_npm_pkgs, updated_npm_pkgs = npm_pkgs.compare_npm_pkgs(new_npm_pkgs_obj)
+            
+            changes = {}
+
+            if deleted_npm_pkgs:
+                changes["deleted_npm_pkgs"] = deleted_npm_pkgs
+            if new_npm_pkgs:
+                changes["new_npm_pkgs"] = new_npm_pkgs
+            if updated_npm_pkgs:
+                changes["updated_npm_pkgs"] = updated_npm_pkgs
+
+            # If any changes happens
+            if changes:
+                # Apply updates
+                data["updated"] = datetime.utcnow
+                npm_pkgs.update(**data)
+
+                # Create a new ChangeLog entry
+                change_log_entry = ChangeLogInstalledNpmPackages(
+                    pip_packages = npm_pkgs.id,
+                    changes = changes
+                )
+                change_log_entry.save()
+            else:
+                # Apply only updated time
+                npm_pkgs.update(updated=datetime.utcnow)
+
+        except Exception as e:
+            return jsonify({'error': e}), 500
+        
+    else:
+        # CREATE If System Installed npm_packages not exist 
+        try:
+            npm_pkgs = InstalledNpmPackages(**data)
+            npm_pkgs.agent = agent
+            npm_pkgs.save()
+        except Exception as e:
+            return jsonify({'error': e}), 500
+
+    return jsonify(
+        {
+            'message': 'Npm packages registered successfully.',
+        }
+    ), 200
 ##############################################################################
