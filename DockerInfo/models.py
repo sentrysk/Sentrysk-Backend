b12007ed86919634e7ba143a60ddf801828c0b74
keraattin <kerem-oruc@outlook.com>
2024-04-24 18:05:55 +0300

    #1 Containers model added to Docker Models

--- DockerInfo/models.py
+++ DockerInfo/models.py
@@ -32,3 +32,28 @@ class Images(EmbeddedDocument):
 
     def __str__(self):
         return str(self.serialize())
+    
+class Containers(EmbeddedDocument):
+    container_id     = StringField()
+    image            = StringField()
+    status           = StringField()
+    ports            = StringField()
+    networks         = StringField()
+    created          = DateTimeField()
+    labels           = StringField()
+
+
+    def serialize(self):
+        data = {
+            "container_id": self.container_id,
+            "image": self.image,
+            "status": self.status,
+            "ports": self.ports,
+            "networks": self.networks,
+            "created": self.created,
+            "labels": self.labels
+        }
+        return data
+
+    def __str__(self):
+        return str(self.serialize())
