02a4155487c8cf883f9d1fc3d988ce3d94a0f6c0
keraattin <kerem-oruc@outlook.com>
2024-04-24 18:08:50 +0300

    #1 Networks model added to Docker Models

--- DockerInfo/models.py
+++ DockerInfo/models.py
@@ -42,7 +42,6 @@ class Containers(EmbeddedDocument):
     created          = DateTimeField()
     labels           = StringField()
 
-
     def serialize(self):
         data = {
             "container_id": self.container_id,
@@ -59,16 +58,35 @@ class Containers(EmbeddedDocument):
         return str(self.serialize())
     
 class Volumes(EmbeddedDocument):
-    volume_name     = StringField()
+    volume_name      = StringField()
     mountpoint       = StringField()
     created          = DateTimeField()
     labels           = StringField()
-
 
     def serialize(self):
         data = {
             "volume_name": self.volume_name,
             "mountpoint": self.mountpoint,
+            "created": self.created,
+            "labels": self.labels
+        }
+        return data
+
+    def __str__(self):
+        return str(self.serialize())
+    
+class Networks(EmbeddedDocument):
+    network_id       = StringField()
+    name             = StringField()
+    driver           = StringField()
+    created          = DateTimeField()
+    labels           = StringField()
+
+    def serialize(self):
+        data = {
+            "network_id": self.network_id,
+            "name": self.name,
+            "driver": self.driver,
             "created": self.created,
             "labels": self.labels
         }
