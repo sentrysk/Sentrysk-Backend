c456a2d32fe94b806d2f27d7d17d95d3f0c256c4
keraattin <kerem-oruc@outlook.com>
2024-04-25 11:08:02 +0300

    #2 Containers Schema Added to Docker Info Schemeas

--- DockerInfo/schema.py
+++ DockerInfo/schema.py
@@ -14,10 +14,23 @@ class ImagesSchema(Schema):
     created      = fields.DateTime(required=True)
     labes        = fields.Str(required=False)
 ##############################################################################
+    
+# Containers Schema
+##############################################################################
+class ContainersSchema(Schema):
+    container_id = fields.Str(required=True)
+    image        = fields.Str(required=True)
+    status       = fields.Str(required=True)
+    ports        = fields.Str(required=False)
+    networks     = fields.Str(required=False)
+    created      = fields.DateTime(required=True)
+    labes        = fields.Str(required=False)
+##############################################################################
 
 # Register Schema
 ##############################################################################
 class RegisterSchema(Schema):
     is_installed    = fields.Boolean(required=True)
     images          = fields.List(fields.Nested(ImagesSchema))
+    containers      = fields.List(fields.Nested(ContainersSchema))
 ##############################################################################
