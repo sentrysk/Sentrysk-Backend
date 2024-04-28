95228791d23ba3d0ef06886a313ea9857214542d
keraattin <kerem-oruc@outlook.com>
2024-04-28 10:13:45 +0300

    #3 Docker Info Register route added

--- DockerInfo/routes.py
+++ DockerInfo/routes.py
@@ -3,10 +3,14 @@
 # Libraries
 ##############################################################################
 from flask import Blueprint, request, jsonify
+from datetime import datetime
+from marshmallow import ValidationError
 
 from .models import DockerInfo
+from .schema import RegisterSchema
 from Shared.validators import agent_token_required, auth_token_required
 
+from Agents.helper_funcs import get_id_by_token
 from Agents.models import Agent
 ##############################################################################
 
@@ -21,7 +25,7 @@ sys_dckr_bp = Blueprint('sys_docker_blueprint', __name__)
 # Get All Docker Infor from All Agents
 @sys_dckr_bp.route('/<agent_id>', methods=['GET'])
 @auth_token_required
-def get_docker_info_by_agent_id(agent_id):
+def get_docker_info_by_agent_id():
     try:
         # Get All Docker Info from DB
         all_docker_infos = DockerInfo.objects()
@@ -42,4 +46,46 @@ def get_docker_info_by_agent_id(agent_id):
     except Exception as e:
         return jsonify({"Message":str(e)}), 404
 
+# Register
+@sys_dckr_bp.route('/', methods=['POST'])
+@agent_token_required
+def register():
+    # Get Agent Token by Authorization Header
+    agent_token = request.headers.get('Authorization')
+    # Get Agent ID by Token
+    agent_id = get_id_by_token(agent_token)
+    # Get Agent by Agent ID
+    agent = Agent.objects(id=agent_id).first()
+
+    try:
+        # Load and validate the JSON request using the schema
+        data = RegisterSchema().load(request.json)
+    except ValidationError as e:
+        # Return validation errors as a JSON response with a 400 status code
+        return jsonify({'error': e.messages}), 400
+        
+    # Get if record already exist
+    docker_info = DockerInfo.objects(agent=agent).first()
+
+    if docker_info:
+        # UPDATE If System Information data already exist
+        try:
+            docker_info.update(updated=datetime.utcnow)
+        except Exception as e:
+            return jsonify({'error': e}), 500
+        
+    else:
+        # CREATE If Docker Info not exist 
+        try:
+            docker_info = DockerInfo(**data)
+            docker_info.agent = agent
+            docker_info.save()
+        except Exception as e:
+            return jsonify({'error': e}), 500
+
+    return jsonify(
+        {
+            'message': 'Docker Info registered successfully.',
+        }
+    ), 200
 ##############################################################################
