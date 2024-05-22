e700000aa5def18f52b8d2e4f65049e05465421e
keraattin <kerem-oruc@outlook.com>
2024-05-22 10:02:33 +0300

    #4 Password update endpoint implemented

--- Users/routes.py
+++ Users/routes.py
@@ -10,7 +10,7 @@ from marshmallow import  ValidationError
 
 from .models import User
 from Session.models import Session
-from .schema import RegisterSchema,LoginSchema
+from .schema import RegisterSchema,LoginSchema,UpdatePasswordSchema
 from Shared.configs import SECRET_KEY,JWT_ALG
 from Shared.validators import auth_token_required
 from .helper_funcs import get_email_by_token
@@ -167,4 +167,50 @@ def get_user_details_by_token():
         return jsonify(user)
     except Exception as e:
         return jsonify({"error":str(e)}), 500
+    
+# Update Current User Password
+@users_bp.route('/updatepass', methods=['POST'])
+@auth_token_required
+def update_current_user_password():
+    try:
+        # Load and validate the JSON request using the schema
+        data = UpdatePasswordSchema().load(request.json)
+    except ValidationError as e:
+        # Return validation errors as a JSON response with a 400 status code
+        return jsonify({'error': e.messages}), 400
+
+    try:
+        # Get JWT Token by Authorization Header
+        jwt_token = request.headers.get('Authorization')
+        # Get Email by Token
+        email = get_email_by_token(jwt_token)
+    except Exception as e:
+        return jsonify({'error': e.messages}), 404
+
+    try:
+        # Get User object by Email and Safe Serialize
+        user = User.objects(email=email).first()
+    except Exception as e:
+        return jsonify({"error":"User not found"})
+    
+
+    current_password    = data.get('current_password')
+    new_password        = data.get('new_password')
+    new_password_repeat = data.get('new_password_repeat')
+
+    if new_password == new_password_repeat:
+        # If Current Password matches
+        if bcrypt.checkpw(current_password.encode('utf-8'), user.password):
+            # Generate password hash from new_password
+            hashed_password = bcrypt.hashpw(new_password.encode('utf-8'), bcrypt.gensalt())
+            # Save Updated Password
+            user.password =  hashed_password  
+            user.save()
+            return jsonify({'message':'Password updated successfully'})
+        else:
+            return jsonify({'error':'Current Password not match'})
+    else:
+        return jsonify({'error':'Password and Repeat not match'})
+        
+
 ##############################################################################
