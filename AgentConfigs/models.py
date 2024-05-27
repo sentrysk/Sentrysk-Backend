7ffae41a7536559636a91d919dbe24ddc56d0da4
keraattin <kerem-oruc@outlook.com>
2024-05-27 13:23:47 +0300

    #5 Agent Configs Model Initiated

---
+++ AgentConfigs/models.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# Libraries
+##############################################################################
+from mongoengine import (
+    Document, DictField, ReferenceField, DateTimeField, ListField,
+    EmbeddedDocument, EmbeddedDocumentField, StringField, IntField
+)
+from datetime import datetime
+##############################################################################
+
+
+# Models
+##############################################################################
+
+##############################################################################
