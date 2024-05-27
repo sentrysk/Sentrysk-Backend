a311efad825f7493e3236440925b07e28263f265
keraattin <kerem-oruc@outlook.com>
2024-05-27 13:24:13 +0300

    #5 Agent Configs Endpoints Added

--- AgentConfigs/models.py
+++ AgentConfigs/models.py
@@ -12,5 +12,15 @@ from datetime import datetime
 
 # Models
 ##############################################################################
+class Endpoints(EmbeddedDocument):
+    system_info        = StringField(required=True)
+    user_info          = StringField(required=True)
+    installed_programs = StringField(required=True)
+    service_info       = StringField(required=True)
+    last_logons        = StringField(required=True)
+    pip_pkgs           = StringField(required=True)
+    npm_pkgs           = StringField(required=True)
+    docker_info        = StringField(required=True)
+
 
 ##############################################################################
