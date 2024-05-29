f4bc420fbb951e4cc606dbd37f9f51ad446bee66
keraattin <kerem-oruc@outlook.com>
2024-05-29 09:53:16 +0300

    #7 Agent Configs Register method initiated and first time create part added

--- AgentConfigs/routes.py
+++ AgentConfigs/routes.py
@@ -6,6 +6,7 @@ from flask import Blueprint, request, jsonify
 import json
 from marshmallow import ValidationError
 
+from .models import AgentConfig
 from .schema import AgentConfigRegisterSchema
 from Shared.validators import auth_token_required
 
@@ -22,6 +23,43 @@ agnt_configs_bp = Blueprint('agent_configs_blueprint', __name__)
 # Routes
 ##############################################################################
 
+# Register
+@agnt_configs_bp.route('/', methods=['POST'])
+@auth_token_required
+def register():
+    # Get Agent Token by Authorization Header
+    agent_token = request.headers.get('Authorization')
+    # Get Agent ID by Token
+    agent_id = get_id_by_token(agent_token)
+    # Get Agent by Agent ID
+    agent = Agent.objects(id=agent_id).first()
+
+    try:
+        # Load and validate the JSON request using the schema
+        data = AgentConfigRegisterSchema().load(request.json)
+    except ValidationError as e:
+        # Return validation errors as a JSON response with a 400 status code
+        return jsonify({'error': e.messages}), 400
+
+    # Get Agent ID by Token
+    agent_config = AgentConfig().objects(agent=agent).first()
+
+    if agent_config:
+        # UPDATE If Agent Config already exist
+        pass
+    else:
+        # CREATE If Agent Config not exist
+        try:
+            agent_config = AgentConfig(**data)
+            agent_config.agent = agent
+            agent_config.save()
+        except Exception as e:
+            return jsonify({'error': e}), 500
 
 
+    return jsonify(
+        {
+            'message': 'Agent Config registered successfully.',
+        }
+    ), 200
 ##############################################################################
