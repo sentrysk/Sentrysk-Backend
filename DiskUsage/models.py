33785905d3d50726fb170b56760fe77d19ef1fae
keraattin <kerem-oruc@outlook.com>
2024-06-21 12:15:49 +0300

    #8 Convert size function added to Disk Usage Model

--- DiskUsage/models.py
+++ DiskUsage/models.py
@@ -33,6 +33,14 @@ class DiskUsage(Document):
         'indexes': ['timestamp']
     }
 
+    def convert_size(self, size_bytes):
+        if size_bytes == 0:
+            return "0B"
+        size_name = ("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB")
+        i = int(math.floor(math.log(size_bytes, 1024)))
+        p = math.pow(1024, i)
+        s = round(size_bytes / p, 2)
+        return f"{s} {size_name[i]}"
 
     # Helper function to serialize DiskUsage objects
     def serialize(self):
