fb748c5e21f8870a1fcd1ca29c8e0832902917ed
keraattin <kerem-oruc@outlook.com>
2024-07-18 15:58:07 +0300

    #15 Memory usage register endpoint added

--- MemoryUsage/routes.py
+++ MemoryUsage/routes.py
@@ -23,5 +23,37 @@ memory_usage_bp = Blueprint('memory_usage_blueprint', __name__)
 # Routes
 ##############################################################################
 
+# Register
+@memory_usage_bp.route('/', methods=['POST'])
+@agent_token_required
+def register():
+    # Get Agent Token by Authorization Header
+    agent_token = request.headers.get('Authorization')
+    # Get Agent ID by Token
+    agent_id = get_id_by_token(agent_token)
+    # Get Agent by Agent ID
+    agent = Agent.objects(id=agent_id).first()
 
+    try:
+        # Load and validate the JSON request using the schema
+        data = RegisterSchema().load(request.json)
+    except ValidationError as e:
+        # Return validation errors as a JSON response with a 400 status code
+        return jsonify({'error': e.messages}), 400
+
+    # Register Disk Usage Data
+    try:
+        memory_usage = MemoryUsage(**data)
+        memory_usage.agent = agent
+        memory_usage.percent = (memory_usage.used_size / memory_usage.total_size) * 100
+        memory_usage.free_size = memory_usage.total_size - memory_usage.used_size
+        memory_usage.save()
+    except Exception as e:
+        return jsonify({'error': e}), 500
+
+    return jsonify(
+        {
+            'message': 'Memory Usage registered successfully.',
+        }
+    ), 200
 ##############################################################################
