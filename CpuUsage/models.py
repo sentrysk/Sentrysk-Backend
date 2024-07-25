59546c14d285ca6dc9edd6c03885279b78b6ae95
keraattin <kerem-oruc@outlook.com>
2024-07-25 15:21:11 +0300

    #18 Cpu Usage model added

---
+++ CpuUsage/models.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+# Libraries
+##############################################################################
+from mongoengine import (
+    Document, ReferenceField, DateTimeField, FloatField
+)
+from datetime import datetime
+
+from Agents.models import Agent
+##############################################################################
+
+
+# Models
+##############################################################################
+class CpuUsage(Document):
+    agent           = ReferenceField(Agent)
+    timestamp       = DateTimeField(required=True,default=datetime.utcnow)
+    cpu_usage       = FloatField(required=True)
+
+    meta = {
+        'indexes': ['timestamp']
+    }
+
+    # Helper function to serialize CpuUsage objects
+    def serialize(self):
+        return {
+            'agent': str(self.agent.id),
+            'timestamp': self.timestamp,
+            'cpu_usage': self.convert_size(self.used_size),
+        }
+    
+    def __str__(self):
+        return str(self.serialize())
+##############################################################################
