b3a20b77dfaa913d5d99ee79c42705c7bedb4dc3
keraattin <kerem-oruc@outlook.com>
2024-07-25 16:45:56 +0300

    #16 Cpu Usage register endpoint added

--- CpuUsage/routes.py
+++ CpuUsage/routes.py
@@ -23,4 +23,35 @@ cpu_usage_bp = Blueprint('cpu_usage_blueprint', __name__)
 # Routes
 ##############################################################################
 
+# Register
+@cpu_usage_bp.route('/', methods=['POST'])
+@agent_token_required
+def register():
+    # Get Agent Token by Authorization Header
+    agent_token = request.headers.get('Authorization')
+    # Get Agent ID by Token
+    agent_id = get_id_by_token(agent_token)
+    # Get Agent by Agent ID
+    agent = Agent.objects(id=agent_id).first()
+
+    try:
+        # Load and validate the JSON request using the schema
+        data = RegisterSchema().load(request.json)
+    except ValidationError as e:
+        # Return validation errors as a JSON response with a 400 status code
+        return jsonify({'error': e.messages}), 400
+
+    # Register CPU Usage Data
+    try:
+        memory_usage = CpuUsage(**data)
+        memory_usage.agent = agent
+        memory_usage.save()
+    except Exception as e:
+        return jsonify({'error': e}), 500
+
+    return jsonify(
+        {
+            'message': 'CPU Usage registered successfully.',
+        }
+    ), 200
 ##############################################################################
