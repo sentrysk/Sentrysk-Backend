2cb581f15a33a3a0764d9dba9e88e4411ae48e4d
keraattin <kerem-oruc@outlook.com>
2024-07-29 15:42:28 +0300

    #18 Cpu usage section typo fixed

--- CpuUsage/models.py
+++ CpuUsage/models.py
@@ -27,7 +27,7 @@ class CpuUsage(Document):
         return {
             'agent': str(self.agent.id),
             'timestamp': self.timestamp,
-            'cpu_usage': self.used_size,
+            'cpu_usage': self.cpu_usage,
         }
     
     def __str__(self):
